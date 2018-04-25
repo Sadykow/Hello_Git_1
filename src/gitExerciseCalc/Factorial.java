5ce116bfc0c1b3218bbdd78eaf47fbadd1899b42
Jodie <jodiemmuller@gmail.com>
2018-04-26 09:04:35 +1000

    Added Factorial code

--- src/gitExerciseCalc/Factorial.java
+++ src/gitExerciseCalc/Factorial.java
@@ -1,5 +1,26 @@
 package gitExerciseCalc;
 
-public class Factorial {
-
+public class Factorial implements Calculator{
+	
+	private int value;
+	
+	/**
+	 * Factorial Constructor
+	 * @param number int
+	 */
+	public Factorial(int number) {
+		value = number;
+	}
+	
+	/**
+	 * Provides the result
+	 */
+	@Override
+	public String getResult() {
+		int sum = 0;
+		for(int factor = 1; factor <= value; factor++) {
+			sum += factor;
+		}
+		return sum + "";
+	}
 }
