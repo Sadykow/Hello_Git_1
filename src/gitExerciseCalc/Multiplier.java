bc5269dd884f57282571f76eca96cb41169ab392
Jodie <jodiemmuller@gmail.com>
2018-04-26 09:20:33 +1000

    Added multiplier class

--- src/gitExerciseCalc/Multiplier.java
+++ src/gitExerciseCalc/Multiplier.java
@@ -12,10 +12,40 @@ public class Multiplier implements Calculator {
 	/* (non-Javadoc)
 	 * @see gitExerciseCalc.Calculator#getResult()
 	 */
+	
+	private double valueA, valueB;
+	private int method;
+	
+	/**
+	 * Multiplier constructor for ints
+	 * @param a int
+	 * @param b int
+	 */
+	public Multiplier(int a, int b) {
+		valueA = a; valueB = b;
+		method = 1;
+	}
+	
+	/**
+	 * Multiplier constructor for doubles
+	 * @param a int
+	 * @param b int
+	 */
+	public Multiplier(double a, double b) {
+		valueA = a; valueB = b;
+		method = 2;
+	}
+	
 	@Override
 	public String getResult() {
-		// TODO Auto-generated method stub
-		return null;
+		switch(method) {
+		case 1:
+			return valueA * valueB + "";
+		case 2:
+			return valueA * valueB + "";
+		default:
+			return null;
+		}
 	}
 
 }
