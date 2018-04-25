279a30c47072c8800c2eeee788bbc1ff9c50975b
Jericho <n9747575@qut.edu.au>
2018-04-26 09:15:59 +1000

    Test3

--- src/gitExerciseCalc/ComplexNumber.java
+++ src/gitExerciseCalc/ComplexNumber.java
@@ -1,6 +1,14 @@
 package gitExerciseCalc;
 
-public class ComplexNumber {
+public class ComplexNumber implements Calculator {
   double real;
   double complex;
+  public ComplexNumber(double real,double complex){
+		this.real=real;
+		this.complex=complex;
+  }
+  @Override
+  public String getResult() {
+	  
+  }
 }
