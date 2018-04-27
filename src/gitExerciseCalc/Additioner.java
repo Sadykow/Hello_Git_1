0d194116d7e4b4cb2ef575702d264d158598b92d
Ben Coates <ben@april9.com.au>
2018-04-27 12:02:52 +1000

    basic functionality implemented

--- src/gitExerciseCalc/Additioner.java
+++ src/gitExerciseCalc/Additioner.java
@@ -9,71 +9,82 @@ package gitExerciseCalc;
  */
 public class Additioner implements Calculator {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see gitExerciseCalc.Calculator#getResult()
 	 */
 	private double valueAlpha, valueBeta;
+	private String stringAlpha, stringBeta;
 	private int method;
-	
+
 	/**
 	 * Constructor deals with integers.
-	 * @param a int
-	 * @param b int
-	 * @param method will define way of performing operation.
+	 * 
+	 * @param a
+	 *            int
+	 * @param b
+	 *            int
+	 * @param method
+	 *            will define way of performing operation.
 	 */
 	public Additioner(int a, int b, int method) {
-		valueAlpha = a; valueBeta = b;
+		this.valueAlpha = a;
+		this.valueBeta = b;
 		this.method = method;
 	}
-	
-	
+
 	/**
 	 * Constructor deals with doubles.
-	 * @param a doubles
-	 * @param b doubles
-	 * @param method will define way of performing operation.
+	 * 
+	 * @param a
+	 *            doubles
+	 * @param b
+	 *            doubles
+	 * @param method
+	 *            will define way of performing operation.
 	 */
 	public Additioner(double a, double b, double method) {
-		valueAlpha = a; valueBeta = b;
+		this.valueAlpha = a;
+		this.valueBeta = b;
 		this.method = (int)method;
 	}
-	
+
 	/**
 	 * Constructor deals with strings.
-	 * @param a doubles
-	 * @param b doubles
-	 * @param method will define way of performing operation.
+	 * 
+	 * @param a
+	 *            doubles
+	 * @param b
+	 *            doubles
+	 * @param method
+	 *            will define way of performing operation.
 	 */
 	public Additioner(String a, String b, String method) {
 		// Develop something
-		/*...*/
+		this.stringAlpha = a;
+		this.stringBeta = b;
 		this.method = Integer.parseInt(method);
 	}
-	
-	/*
-	 * Develop your methods...
-	 */
-	 
-	 public void useless(void){
-		int case = 4;
-		for(int i=case-1; i>1,i--){
-			case=case*i;
-		}
-		
-	 }
-	 
-	
+
 	/**
 	 * Use cases to switch between your methods
 	 */
 	@Override
 	public String getResult() {
-		switch(method) {
-		case 1:
-			return valueAlpha + valueBeta +"";
-			default:
-				return null;
-		}		
+		switch (method) {
+		case 1: // integers
+			return String.valueOf((double)((int)this.valueAlpha + (int)this.valueBeta));
+			
+		case 2: // doubles 
+			return String.valueOf((double)this.valueAlpha + (double)this.valueBeta);
+		
+		case 3: // string concat
+			return this.stringAlpha + this.stringBeta;
+			
+		default:
+			return null;
+		}
 	}
 
 }
