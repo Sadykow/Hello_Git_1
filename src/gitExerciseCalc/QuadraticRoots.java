22c77d8694c26df934b23e52c28ea5da34c804eb
NoahDavey <NoahDavey@users.noreply.github.com>
2018-04-27 11:32:10 +1000

    test

---
+++ src/gitExerciseCalc/QuadraticRoots.java
@@ -0,0 +1,25 @@
+package gitExerciseCalc;
+
+public class QuadraticRoots {
+
+	int a,b,c;
+	
+	public QuadraticRoots(int a, int b, int c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double x1() {
+		
+	}
+	
+	public double x2() {
+		
+	}
+	
+	public void changeCoefficients() {
+		
+	}
+	
+}
